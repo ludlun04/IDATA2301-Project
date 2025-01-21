fb1c1fdc9e1c3d714205af02fd51ba8f258eb848
stian oye <stianoyejenssen@gmail.com>
2025-01-21 13:34:30 +0100

    Started work on rent page

--- src/pages/rent/Rent.js
+++ src/pages/rent/Rent.js
@@ -1,7 +1,21 @@
 import "./Rent.css"
+import bmw from "./../../resources/images/bmw.jpg";
 
 export default function Rent() {
     return (
-        <div className={"Rent"}>RentPage</div>
+        <div className={"Rent"}>
+          <div>
+            <img alt="" className={"CarImage"} src={bmw}/>
+            <h1>Hello World!</h1>
+            <h2>Attributes</h2>
+            <section className={"Attributes"}></section>
+            <h2>Description</h2>
+            <p>This is a description for this car</p>
+          </div>
+          
+          <div>
+            <p>Other stuff</p>
+          </div>
+        </div>
     )
 }
