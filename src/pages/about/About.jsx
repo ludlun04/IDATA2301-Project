d9a7a52fdf1b663150585117c81835a53d47495f
Ludvik Lund-Hole <ludvik.lund.hole@gmail.com>
2025-01-21 18:53:47 +0100

    styling employee card and about page

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -5,11 +5,18 @@ export default function About() {
     return (
         <div className={"About"}>
             <div className={"aboutOurStory"}>
-                <h1>Our Story</h1>
-                <p>This is very cool</p>
+                <h1 className={"aboutHeader"}>Our Story</h1>
+                <p className={"aboutOurStoryParagraph"}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum.
+                </p>
             </div>
             <div className={"aboutEmployees"}>
-                <h1>Employees</h1>
+                <h1 className={"aboutHeader"}>Employees</h1>
                 <div className={"aboutEmployeeCardContainer"}>
                     <EmployeeCard></EmployeeCard>
                     <EmployeeCard></EmployeeCard>
