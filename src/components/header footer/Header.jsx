5aace809d274aaed7f422f34c45aaee5280381a0
Kristian Garder <kristiangarder@dhcp-10-24-42-27.wlan.ntnu.no>
2025-01-27 12:04:56 +0100

    Header menu button update and animations

--- src/components/header footer/Header.jsx
+++ src/components/header footer/Header.jsx
@@ -13,12 +13,16 @@ export default function Header() {
         <div className={"Header"}>
             <NavLink className={"logo"} to={"/home"}>Rental Roulette</NavLink>
             <div className={`headerRightContainer ${menuButtonActive ? "active" : ""}`}>
-                <NavLink className={"navLink rent"} to={"/portal"}>Rent</NavLink>
-                <NavLink className={"navLink"} to={"/about"}>About</NavLink>
-                <NavLink className={"navLink"} to={"contact"}>Contact</NavLink>
-                <NavLink className={"navLink sign-in"} to={"/sign-in"}>Sign In</NavLink>
+                <NavLink className={"navLink rent"} to={"/portal"} onClick={handleToggle}>Rent</NavLink>
+                <NavLink className={"navLink"} to={"/about"} onClick={handleToggle}>About</NavLink>
+                <NavLink className={"navLink"} to={"contact"} onClick={handleToggle}>Contact</NavLink>
+                <NavLink className={"navLink sign-in"} to={"/sign-in"} onClick={handleToggle}>Sign In</NavLink>
             </div>
-            <input className={"headerMenuButton"} type={"checkbox"} checked={menuButtonActive} onChange={handleToggle}></input>
+            <div className={`headerMenuButtonContainer ${menuButtonActive ? "active" : ""}`} onClick={handleToggle}>
+                <div className={`headerMenuButton top ${menuButtonActive ? "active" : ""}`}></div>
+                <div className={`headerMenuButton middle ${menuButtonActive ? "active" : ""}`}></div>
+                <div className={`headerMenuButton bottom ${menuButtonActive ? "active" : ""}`}></div>
+            </div>
         </div>
     )
 }
