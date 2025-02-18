ef25d29fe1799d6a7cab2d44cc6f796470302f0c
stian oye <stianoyejenssen@gmail.com>
2025-02-18 14:01:06 +0100

    Added gap between car elements in car list on portal page

--- src/components/FilterSection/FilterSection.jsx
+++ src/components/FilterSection/FilterSection.jsx
@@ -5,7 +5,25 @@ const FilterSection = () => {
   return (
     <div className={"FilterSection"}>
       <h2>Filters</h2>
-      <DropdownMenu title={"Something"}>
+      <DropdownMenu title={"First"}>
+      <ul>
+        <li className="FilterLine" ><p>First filter</p> <input type="checkbox"></input></li>
+        <li className="FilterLine" ><p>This is a filter that is very very very long</p> <input type="checkbox"></input></li>
+      </ul>
+      </DropdownMenu>
+      <DropdownMenu title={"Second"}>
+      <ul>
+        <li className="FilterLine" ><p>First filter</p> <input type="checkbox"></input></li>
+        <li className="FilterLine" ><p>This is a filter that is very very very long</p> <input type="checkbox"></input></li>
+      </ul>
+      </DropdownMenu>
+      <DropdownMenu title={"Third"}>
+      <ul>
+        <li className="FilterLine" ><p>First filter</p> <input type="checkbox"></input></li>
+        <li className="FilterLine" ><p>This is a filter that is very very very long</p> <input type="checkbox"></input></li>
+      </ul>
+      </DropdownMenu>
+      <DropdownMenu title={"Fourth"}>
       <ul>
         <li className="FilterLine" ><p>First filter</p> <input type="checkbox"></input></li>
         <li className="FilterLine" ><p>This is a filter that is very very very long</p> <input type="checkbox"></input></li>
