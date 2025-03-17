b2cea7eacecb32815b5215bccc2326453db2d20a
Kristian <garderkristian@gmail.com>
2025-03-17 11:52:27 +0100

    Contact Page final touches

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -3,18 +3,26 @@ import "./Contact.css"
 export default function Contact() {
   return (
     <div className={"Contact"}>
-      <h1>Contact Us</h1>
-      <p>If you have any questions please shoot us a message or come by our office 🚘</p>
+      <h1 className={"ContactHeader"}>Contact Us</h1>
+      <p className={"ContactMessage"}>If you have any questions please send us a message or come by our office 🚘</p>
       <form className={"contact-form"}>
-        <label htmlFor="fname">Name:</label><br />
-        <input className={"contactInput name"} type="text" id="name" name="fname" /><br />
-        <label htmlFor="lname">Email:</label><br />
-        <input className={"contactInput email"} type="text" id="email" name="lname" /><br />
-        <label htmlFor="lname">Subject:</label><br />
-        <input className={"contactInput subject"} type="text" id="subject" name="lname" /><br />
-        <label htmlFor="lname">Message:</label><br />
-        <textarea className={"contactInput message"} type="text" id="message" name="lname" /><br />
-        <input type="submit" value="Submit" />
+          <div className={"form-group"}>
+              <label htmlFor="fname">Name*</label><br />
+              <input className={"contactInput name"} type="text" id="name" name="fname" /><br />
+          </div>
+          <div className={"form-group"}>
+              <label htmlFor="lname">Email*</label><br />
+              <input className={"contactInput email"} type="text" id="email" name="lname" /><br />
+          </div>
+          <div className={"form-group"}>
+              <label htmlFor="lname">Subject*</label><br />
+              <input className={"contactInput subject"} type="text" id="subject" name="lname" /><br />
+          </div>
+          <div className={"form-group"}>
+              <label htmlFor="lname">Message*</label><br />
+              <textarea className={"contactInput message"} type="text" id="message" name="lname" /><br />
+          </div>
+        <input className={"ContactSubmit"} type="submit" value="Send" />
       </form>
     </div>
   )
