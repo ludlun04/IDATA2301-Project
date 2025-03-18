a42cc188bc7de71f3bb162b75523b9fd66c6cba3
Kristian <garderkristian@gmail.com>
2025-03-18 15:06:05 +0100

    Placeholders signup

--- src/pages/signUp/SignUp.jsx
+++ src/pages/signUp/SignUp.jsx
@@ -9,30 +9,30 @@ export default function SignUp() {
             <form className={"signup-form"}>
                 <div className={"form-group"}>
                     <label htmlFor="email">First Name*</label>
-                    <input name="firstName" id="firstName" />
+                    <input name="firstName" id="firstName" placeholder={"Ada"}/>
                 </div>
                 <div className={"form-group"}>
                     <label htmlFor="lastName">Last Name*</label>
-                    <input name="lastName" id="lastName" />
+                    <input name="lastName" id="lastName" placeholder={"Lovelace"}/>
                 </div>
                 <div className={"form-group"}>
                     <label htmlFor="email">Email*</label>
-                    <input name="email" id="email" />
+                    <input name="email" id="email" placeholder={"email@gmail.com"}/>
                 </div>
 
                 <div className={"addressGroup"}>
                     <div className={"form-group"}>
                         <label htmlFor="address">Address*</label>
-                        <input name="address" id="address" />
+                        <input name="address" id="address" placeholder={"Borgundvegen 222A"}/>
                     </div>
                     <div className={"addressCountryZipCodeGroup"}>
                         <div className={"form-group"}>
                             <label htmlFor={"country"}>Country*</label>
-                            <input name="country" id="country" />
+                            <input name="country" id="country" placeholder={"Norway"}/>
                         </div>
                         <div className={"form-group"}>
                             <label htmlFor={"zipCode"}>Zip Code*</label>
-                            <input name="zipCode" id="zipCode" />
+                            <input name="zipCode" id="zipCode" placeholder={"6002"}/>
                         </div>
                     </div>
                 </div>
@@ -40,11 +40,11 @@ export default function SignUp() {
                 <div className={"phoneNumberGroup"}>
                     <div className={"form-group"}>
                         <label htmlFor={"countryCode*"}>Country Code*</label>
-                        <input name={"countryCode"} id="countryCode" />
+                        <input name={"countryCode"} id="countryCode" placeholder={"+47"}/>
                     </div>
                     <div className={"form-group phoneNumber"}>
                         <label htmlFor={"phoneNumber"}>Phone Number*</label>
-                        <input name={"phoneNumber"} id="phoneNumber" />
+                        <input name={"phoneNumber"} id="phoneNumber" placeholder={"800 80 000"}/>
                     </div>
                 </div>
 
