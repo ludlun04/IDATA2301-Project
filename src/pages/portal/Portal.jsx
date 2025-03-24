78daf34dcdbabfac92b760a9cbae8f35478739b1
Kristian <garderkristian@gmail.com>
2025-03-24 11:00:12 +0100

    Implemented favorite star in CarCard

--- src/pages/portal/Portal.jsx
+++ src/pages/portal/Portal.jsx
@@ -28,8 +28,8 @@ export default function Portal() {
                 <CarSearchSortSection onChange={handleUserChoice}/>
                 <div className={"portalCarCards"}>
 
-                    <CarCard price={5234} availability={false} seats={2} year={2002} name={"BMW M3"} company={"Kacper Rentals AS"} />
-                    <CarCard price={4325} availability={true} seats={4} year={2021} name={"Mazda CX3"} company={"Steike Rentals"} />
+                    <CarCard price={5234} availability={false} seats={2} year={2002} name={"BMW M3"} company={"Kacper Rentals AS"} isFavorite={false}/>
+                    <CarCard price={4325} availability={true} seats={4} year={2021} name={"Mazda CX3"} company={"Steike Rentals"} isFavorite={true}/>
                 </div>
             </div>
 
