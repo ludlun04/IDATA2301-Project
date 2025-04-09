72b44acabb1e2778f388f6e7f90b6354b8483c04
stian oye <stianoyejenssen@gmail.com>
2025-04-09 10:34:44 +0200

    Added Dialouge to users page

---
+++ src/components/DashboardComponents/Admin/EditUserDialogue.jsx
@@ -0,0 +1,55 @@
+import "./EditUserDialogue.css"
+
+export default function EditUserDialogue(props) {
+  const user = props.user;
+
+  const onEdit = (e) => {
+    e.preventDefault();
+    console.log("Edit button clicked");
+  }
+
+  const onCancelEdit = (e) => {
+    e.preventDefault();
+    props.onClose();
+  }
+
+  return (
+    <div className="EditUserDialogue">
+      <form >
+        <p>Hello</p>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input  type="text" defaultValue={user.getFirstName()} />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="text" defaultValue={user.getEmail()} />
+        </div>
+        <div>
+          <label htmlFor="phone">Phone Number</label>
+          <input type="text" defaultValue={user.getPhoneNumber()} />
+        </div>
+        <div>
+          <label htmlFor="firstName">First Name</label>
+          <input type="text" defaultValue={user.getFirstName()} />
+        </div>
+        <div>
+          <label htmlFor="lastName">Last Name</label>
+          <input type="text" defaultValue={user.getLastName()} />
+        </div>
+        <div>
+          <label htmlFor="address">Address</label>
+          <input type="text"/>
+        </div>
+        <div>
+          <label htmlFor="birthdate">Birthdate</label>
+          <input type="text" defaultValue ={user.getDateOfBirth()} />
+        </div>
+        <div className={"detailsSectionButtonContainer"}>
+          <button className={"FormSubmitButton detailsSectionButton"} onClick={onEdit}>Edit</button>
+          <button className={"FormSubmitButton detailsSectionButton"} onClick={onCancelEdit}>Cancel</button>
+        </div>
+      </form>
+    </div>
+  );
+}
--- src/components/DashboardComponents/Admin/UserRow.jsx
+++ src/components/DashboardComponents/Admin/UserRow.jsx
@@ -1,22 +1,32 @@
+import { createPortal } from "react-dom"
+import EditUserDialogue from "./EditUserDialogue";
 import "./UserRow.css"
+import { useState } from "react";
 
 export default function UserRow({ user }) {
+  const [portal, setPortal] = useState(<div></div>);
 
+  let onClose = () => {
+    setPortal(<div></div>);
+  }
 
   let editUser = () => {
-    alert(`Editing User ${user.getId()}`)
+    setPortal(createPortal(<EditUserDialogue onClose={onClose} user={user} />, document.body));
   }
 
   return (
-    <tr className={"userRow"}>
-      <td>{user.getId()}</td>
-      <td>{user.getEmail()}</td>
-      <td>{user.getFirstName()}</td>
-      <td>{user.getLastName()}</td>
-      <td>{user.getPhoneNumber()}</td>
-      <td>{user.getDateOfBirth()}</td>
-      <td><div>{user.getRoles().map(role => <p key={role}>{role}</p>)}</div></td>
-      <td><button onClick={editUser}>{"Edit"}</button></td>
-    </tr>
+    <>
+      {portal}
+      <tr className={"userRow"}>
+        <td>{user.getId()}</td>
+        <td>{user.getEmail()}</td>
+        <td>{user.getFirstName()}</td>
+        <td>{user.getLastName()}</td>
+        <td>{user.getPhoneNumber()}</td>
+        <td>{user.getDateOfBirth()}</td>
+        <td><div>{user.getRoles().map(role => <p key={role}>{role}</p>)}</div></td>
+        <td><button onClick={editUser}>{"Edit"}</button></td>
+      </tr>
+    </>
   )
 }
