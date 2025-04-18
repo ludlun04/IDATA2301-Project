269bf316e2423b7c50d5c4385ecda7cc0592fd93
Ludvik <ludvik.lund.hole@gmail.com>
2025-04-18 19:58:37 +0200

    CarsAPI get all cars, CarCard now uses a Car object, portal using API

--- src/api/CarsAPI.js
+++ src/api/CarsAPI.js
@@ -2,33 +2,25 @@ import Constants from "../Constants.jsx";
 import axios from 'axios';
 import { Authentication } from './Authentication';
 import { Car } from '../model/Car';
+import {CarModel} from "../model/CarModel";
+import {CarBrand} from "../model/CarBrand";
+import {TransmissionType} from "../model/TransmissionType";
+import {FuelType} from "../model/FuelType";
+import {Feature} from "../model/Feature";
+import {Addon} from "../model/Addon";
 
 export const CarsAPI = {
 
   getAllCars: async () => {
 
-    const token = Authentication.getToken();
-    const result = await axios.get(`${Constants.API_URL}/car`, {
-      headers: {
-        'Authorization': `Bearer ${token}`
-      }
-    });
+    const result = await axios.get(`${Constants.API_URL}/car`, {});
 
     let cars = [];
 
     for (let i = 0; i < result.data.length; i++) {
-      const car = result.data[i];
-      cars.push(new Car(
-        car.id,
-        car.year,
-        car.numberOfSeats,
-        car.pricePerDay,
-        car.model,
-        car.fuelType,
-        car.transmissionType,
-        car.addons,
-        car.features
-      ));
+      const carObject = result.data[i];
+      const car = CarsAPI.getCarFromJsonObject(carObject);
+      cars.push(car);
     }
 
     console.log(result.data);
@@ -36,5 +28,39 @@ export const CarsAPI = {
     return cars;
 
 
+  },
+
+  getCarFromJsonObject(carObject) {
+    const modelObject = carObject.model;
+
+    const brandObject = modelObject.brand;
+    const brand = new CarBrand(brandObject.id, brandObject.name);
+
+    const model = new CarModel(modelObject.id, modelObject.name, brand);
+
+    const transmissionTypeObject = carObject.transmissionType;
+    const transmissionType = new TransmissionType(transmissionTypeObject.id, transmissionTypeObject.name);
+
+    const fuelTypeObject = carObject.fuelType;
+    const fuelType = new FuelType(fuelTypeObject.id, fuelTypeObject.name);
+
+    const features = carObject.features.map(feature => {
+      return new Feature(feature.id, feature.name);
+    });
+
+    const addons = carObject.addons.map(addon => {
+      return new Addon(addon.id, addon.name);
+    });
+    return new Car(
+      carObject.id,
+      carObject.year,
+      carObject.numberOfSeats,
+      carObject.pricePerDay,
+      model,
+      fuelType,
+      transmissionType,
+      addons,
+      features
+    );
   }
-}
+};
