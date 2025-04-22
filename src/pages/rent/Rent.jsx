5902310d35fc50b21622f3b5c24fef12839ccd19
stian oye <stianoyejenssen@gmail.com>
2025-04-22 19:18:32 +0200

    Started rework

--- src/pages/rent/Rent.jsx
+++ src/pages/rent/Rent.jsx
@@ -2,9 +2,9 @@ import "./Rent.css"
 import CarAttribute from "../../components/CarAttribute/CarAttribute";
 import bmw from "./../../resources/images/bmw.jpg";
 import DatePicker from "react-datepicker";
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import {CarAPI} from "../../api/CarAPI";
+import { CarAPI } from "../../api/CarAPI";
 import CarAttributes from "../../components/CarAttribute/CarAttributes";
 import Loader from "../../components/loader/Loader";
 
@@ -24,7 +24,7 @@ export default function Rent(props) {
   const amountOfDays = Math.ceil((endDate - startDate) / (1000 * 60 * 60 * 24));
   const totalPrice = amountOfDays * carPricePerDay;
 
-  useEffect( () => {
+  useEffect(() => {
     const fetchCar = async () => {
       try {
         const car = await CarAPI.getCar(id);
@@ -47,66 +47,68 @@ export default function Rent(props) {
 
   if (loading) {
     return (
-        <main className={"RentMain"}>
-          <Loader/>
-        </main>
+      <main className={"RentMain"}>
+        <Loader />
+      </main>
     );
   }
   return (
-    <main className="RentMain">
-      <img alt="" className={"RentCarImage"} src={bmw} />
-      <div className="RentInformation">
-        <h1>{car.getName()}</h1>
+    <main className={"RentMain"}>
+      <div>
+        <img alt="" className={"RentCarImage"} src={bmw} />
+
+        <section className={"RentInformation"}>
+          <h1>{car.getName()}</h1>
+        </section>
 
         <section className={"RentAttributes"}>
           <div className={"RentAttributeList"}>
             <CarAttributes year={car.getYear()} seats={car.getNumberOfSeats()} transmission={car.getTransmissionType().getName()} fuel={car.getFuelType().getName()} />
           </div>
         </section>
-      </div>
 
-      <section className={"RentFeatureSection"}>
-        <h2>Features</h2>
-        <div className={"RentFeatureList"}>
-          {features.map((feature, index) => (
+        <section className={"RentFeatureSection"}>
+          <h2>Features</h2>
+          <div className={"RentFeatureList"}>
+            {features.map((feature, index) => (
               <p className={"RentFeatureItem"}>- {feature.getName()}</p>
-          ))}
-        </div>
-      </section>
+            ))}
+          </div>
+        </section>
 
-      <section className={"RentCarDescription"}>
-        <h2>Description</h2>
-        <p>
-          {car.getDescription()}
-          </p>
-      </section>
-
-      <div className={"RentInteraction"}>
-        <div className={"RentInteractionInner"}>
+        <section className={"RentInteraction"}>
           <div className={"RentCompanyCard"}>
             <img className={"RentCompanyImage"} src={bmw} />
             <h2>{car.getCompanyName()}</h2>
           </div>
 
           <div className={"RentDuration"}>
-            <div className="RentDurationSelection">
-              <DatePicker /*monthsShown={3}*/ className={"filtersSectionDatePicker"} selected={startDate} onChange={(date) => setStartDate(date)} dateFormat={dateFormat} portalId={portalId} calendarStartDay={calendarStartDay}/>
-              <DatePicker className={"filtersSectionDatePicker"} selected={endDate} onChange={(date) => setEndDate(date)} dateFormat={dateFormat} portalId={portalId} calendarStartDay={calendarStartDay}/>
+            <div className={"RentDurationSelection"}>
+              <DatePicker /*monthsShown={3}*/ className={"filtersSectionDatePicker"} selected={startDate} onChange={(date) => setStartDate(date)} dateFormat={dateFormat} portalId={portalId} calendarStartDay={calendarStartDay} />
+              <DatePicker className={"filtersSectionDatePicker"} selected={endDate} onChange={(date) => setEndDate(date)} dateFormat={dateFormat} portalId={portalId} calendarStartDay={calendarStartDay} />
             </div>
           </div>
 
-          <div className="RentDailyPrice">
-            <p className="RentTitle">kr/day</p>
-            <p className="RentContent">{carPricePerDay}</p>
+          <div className={"RentDailyPrice"}>
+            <p className={"RentTitle"}>kr/day</p>
+            <p className={"RentContent"}>{carPricePerDay}</p>
           </div>
 
-          <div className="RentTotalPrice">
-            <p className="RentTitle">Total</p>
-            <p className="RentContent">{totalPrice} kr</p>
+          <div className={"RentTotalPrice"}>
+            <p className={"RentTitle"}>Total</p>
+            <p className={"RentContent"}>{totalPrice} kr</p>
           </div>
 
-          <button className="RentButton" type="button">Rent</button>
-        </div>
+          <button className={"RentButton"} type="button">Rent</button>
+        </section>
+
+
+        <section className={"RentCarDescription"}>
+          <h2>Description</h2>
+          <p>
+            {car.getDescription()}
+          </p>
+        </section>
       </div>
     </main>
   )
