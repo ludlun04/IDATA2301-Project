9aeeb162d2fdc48c73da9df670066bbb7ad18278
Ludvik <ludvik.lund.hole@gmail.com>
2025-04-25 10:23:43 +0200

    to date is now always after from date

--- src/components/FilterSection/FiltersSection.jsx
+++ src/components/FilterSection/FiltersSection.jsx
@@ -1,6 +1,6 @@
 import DropdownMenu from "../DropdownMenu/DropdownMenu";
 import FromToSection from "../FromToSection/FromToSection";
-import React, { useState } from "react";
+import React, {useState} from "react";
 import DatePicker from "react-datepicker";
 
 import "react-datepicker/dist/react-datepicker.css";
@@ -15,6 +15,26 @@ const FiltersSection = (props) => {
   const availableFuelTypes = props.fuelTypes;
   const availableSellers = props.sellers;
   const availableSeats = props.seats;
+
+  const oneDayAfter = (date) => {
+    const days = 1;
+    return new Date(date.getTime() + days * 24 * 60 * 60 * 1000);
+  }
+
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(oneDayAfter(startDate));
+
+  // datepicker configuration
+  const dateFormat = "dd.MM.yyyy"; // displayed date format in datepicker
+  const portalId = "root-portal"; // makes the datepicker window not affect positioning of datepicker field
+  const calendarStartDay = 1; // monday as first day of week instead of sunday
+
+  const handleStartDateChange = (date) => {
+    setStartDate(date)
+    if (date >= endDate) {
+      setEndDate(oneDayAfter(date));
+    }
+  }
 
   const alterChosenSellers = (event) => {
     const seller = event.target.value;
@@ -52,65 +72,77 @@ const FiltersSection = (props) => {
     }
   }
 
-
-  const [startDate, setStartDate] = useState(new Date());
-  const [endDate, setEndDate] = useState(new Date());
-
-  // datepicker configuration
-  const dateFormat = "dd.MM.yyyy"; // displayed date format in datepicker
-  const portalId = "root-portal"; // makes the datepicker window not affect positioning of datepicker field
-  const calendarStartDay = 1; // monday as first day of week instead of sunday
-
-
-
   const getHeader = (title) => (
     <h2>{title}</h2>
   )
 
   return (
-    <div className={props.className} style={props.style} ref={props.ref} >
+    <div className={props.className} style={props.style} ref={props.ref}>
       <div className={"FiltersSection"}>
         <section>
           <h1>The car</h1>
           <DropdownMenu alwaysShownContent={getHeader("Manufacturer")}>
             <ul>
               {availableManufacturers.map((manufacturer, index) => (
-                <li key={index} className="filtersSectionFilterLine" ><p>{manufacturer.getName()}</p> <input value={manufacturer.getName()} type="checkbox" onChange={alterChosenManufacturers}></input></li>
+                <li key={index} className="filtersSectionFilterLine"><p>{manufacturer.getName()}</p>
+                  <input value={manufacturer.getName()} type="checkbox"
+                         onChange={alterChosenManufacturers}></input></li>
               ))}
             </ul>
           </DropdownMenu>
           <DropdownMenu alwaysShownContent={getHeader("Fuel type")}>
             <ul>
               {availableFuelTypes.map((fuelType, index) => (
-                <li key={index} className="filtersSectionFilterLine" ><p>{fuelType.getName()}</p> <input value={fuelType.getName()} type="checkbox" onChange={alterChosenFuelTypes}></input></li>
+                <li key={index} className="filtersSectionFilterLine"><p>{fuelType.getName()}</p>
+                  <input value={fuelType.getName()} type="checkbox"
+                         onChange={alterChosenFuelTypes}></input></li>
               ))}
             </ul>
           </DropdownMenu>
           <DropdownMenu alwaysShownContent={getHeader("Seller")}>
             <ul>
               {availableSellers.map((seller, index) => (
-                <li key={index} className="filtersSectionFilterLine" ><p>{seller.getName()}</p> <input value={seller.getName()} type="checkbox" onChange={alterChosenSellers}></input></li>
+                <li key={index} className="filtersSectionFilterLine"><p>{seller.getName()}</p>
+                  <input value={seller.getName()} type="checkbox"
+                         onChange={alterChosenSellers}></input></li>
               ))}
             </ul>
           </DropdownMenu>
           <DropdownMenu alwaysShownContent={getHeader("Seats")}>
             <ul>
               {availableSeats.map((seat, index) => (
-                <li key={index} className="filtersSectionFilterLine" ><p>{seat}</p> <input value={seat} type="checkbox" onChange={alterChosenSeats}></input></li>
+                <li key={index} className="filtersSectionFilterLine"><p>{seat}</p> <input
+                  value={seat} type="checkbox" onChange={alterChosenSeats}></input></li>
               ))}
             </ul>
           </DropdownMenu>
         </section>
         <section>
           <h1>The Time</h1>
-          <li className="filtersSectionFilterLine" ><p>Available now</p> <input type="checkbox"></input></li>
+          <li className="filtersSectionFilterLine"><p>Available now</p> <input
+            type="checkbox"></input></li>
           <section className={"filtersSectionDateLine"}>
             <p>From</p>
-            <DatePicker /*monthsShown={3}*/ className={"filtersSectionDatePicker"} selected={startDate} onChange={(date) => setStartDate(date)} dateFormat={dateFormat} portalId={portalId} calendarStartDay={calendarStartDay}/>
+            <DatePicker
+              className={"filtersSectionDatePicker"}
+              selected={startDate}
+              minDate={new Date()}
+              onChange={(date) => handleStartDateChange(date)}
+              dateFormat={dateFormat}
+              portalId={portalId}
+              calendarStartDay={calendarStartDay}
+            />
           </section>
           <section className={"filtersSectionDateLine"}>
             <p>To</p>
-            <DatePicker className={"filtersSectionDatePicker"} selected={endDate} onChange={(date) => setEndDate(date)} dateFormat={dateFormat} portalId={portalId} calendarStartDay={calendarStartDay}/>
+            <DatePicker
+              className={"filtersSectionDatePicker"}
+              selected={endDate}
+              minDate={oneDayAfter(startDate)}
+              onChange={(date) => setEndDate(date)}
+              dateFormat={dateFormat} portalId={portalId}
+              calendarStartDay={calendarStartDay}
+            />
           </section>
 
         </section>
@@ -120,7 +152,9 @@ const FiltersSection = (props) => {
 
         </section>
 
-        <button className={"FormSubmitButton"} id={"filtersSectionFormSubmitButton"} onClick={props.onSave}>Save</button>
+        <button className={"FormSubmitButton"} id={"filtersSectionFormSubmitButton"}
+                onClick={props.onSave}>Save
+        </button>
       </div>
     </div>
   )
