96180e52510baf33d31724555bed701b023f5690
Kristian <garderkristian@gmail.com>
2025-05-02 12:46:40 +0200

    Added home button to 404 page

--- src/pages/404/Page404.jsx
+++ src/pages/404/Page404.jsx
@@ -1,13 +1,21 @@
 import img from "../../resources/images/404/404ErrorImage.png"
 import "./Page404.css"
+import {useNavigate} from "react-router-dom";
 
 const Page404 = () => {
+    const navigate = useNavigate();
+
+    const onClick = () => {
+        navigate("/")
+    }
+
     return (
         <>
             <main className={"main404"}>
                 <h1>Error 404</h1>
                 <h2>Page not found!</h2>
-                <img src={img} />
+                <img src={img}  alt={"Crashed car image"}/>
+                <button onClick={onClick}>Back to home</button>
             </main>
         </>
     )
