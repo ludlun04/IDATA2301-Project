c0ae7a9a3a45f7fb59625655e2fe66e3f49692dd
stian oye <stianoyejenssen@gmail.com>
2025-05-09 12:14:57 +0200

    Added documentation to carBrand api and carurlbuilder

--- src/api/CarBrandAPI.js
+++ src/api/CarBrandAPI.js
@@ -2,8 +2,18 @@ import Constants from "../Constants";
 import axios from "axios";
 import {CarBrand} from "../model/CarBrand";
 
+/**
+ * Apis for handling car brands.
+ * It provides methods to fetch car brands.
+ */
 export const CarBrandAPI = {
 
+  /**
+   * Fetching all car brands in use.
+   *
+   * @returns {Promise<CarBrand[]>} A promise that resolves to an array of CarBrand objects.
+   * @throws {Error} If there is an error fetching the brands.
+   */
   getBrandsUsedInCars: async () => {
     try {
       const result = await axios.get(`${Constants.API_URL}/brand/with_rentals`, {});
@@ -21,6 +31,12 @@ export const CarBrandAPI = {
   }
 }
 
+/**
+ * Converts a JSON representation of a brand to a CarBrand object.
+ * @param {Object} brandObject - The JSON object representing the brand.
+ * @returns {CarBrand} A CarBrand object.
+ * @throws {Error} If the brandObject is not valid.
+ */
 const getBrandFromJsonObject = (brandObject) => {
   try {
     return new CarBrand(brandObject.id, brandObject.name);
--- src/api/CarUrlBuilder.js
+++ src/api/CarUrlBuilder.js
@@ -1,5 +1,9 @@
 import Constants from "../Constants";
 
+/**
+ * Class to build URLs for car-related API requests.
+ * It provides methods to add various filters to the URL.
+ */
 export class CarUrlBuilder {
 
   CAR_URL = Constants.API_URL + "/car";
@@ -14,9 +18,16 @@ export class CarUrlBuilder {
   FROM_PRICE_FILTER = "from_price";
   TO_PRICE_FILTER = "to_price";
 
+  /** Constructor for builder */
   constructor() {
     this._url = this.CAR_URL;
   }
+
+  /**
+   * Sets manufacturers to be used in the URL.
+   * @param {string[]} manufacturers list of manufacturers
+   * @returns {CarUrlBuilder} the builder instance
+   */
   withManufacturers(manufacturers) {
     if (manufacturers.length === 0) {
       return this;
@@ -26,6 +37,11 @@ export class CarUrlBuilder {
     return this;
   }
 
+  /**
+   * Sets fuel types to be used in the URL.
+   * @param {string[]} fuelTypes list of fuel types
+   * @returns {CarUrlBuilder} the builder instance
+   */
   withFuelTypes(fuelTypes) {
     if (fuelTypes.length === 0) {
       return this;
@@ -35,6 +51,11 @@ export class CarUrlBuilder {
     return this;
   }
 
+  /**
+   * Sets sellers to be used in the URL.
+   * @param {string[]} sellers list of sellers
+   * @returns {CarUrlBuilder} the builder instance
+   */
   withSellers(sellers) {
     if (sellers.length === 0) {
       return this;
@@ -44,6 +65,11 @@ export class CarUrlBuilder {
     return this;
   }
 
+  /**
+   * Sets seats to be used in the URL.
+   * @param {number[]} seats list of seats
+   * @returns {CarUrlBuilder} the builder instance
+   */
   withSeats(seats) {
     if (seats.length === 0) {
       return this;
@@ -53,6 +79,11 @@ export class CarUrlBuilder {
     return this;
   }
 
+  /**
+   * Sets keyword to be used in the URL.
+   * @param {string} keyword the keyword
+   * @returns {CarUrlBuilder} the builder instance
+   */
   withKeyword(keyword) {
     if (keyword.length === 0) {
       return this;
@@ -62,6 +93,12 @@ export class CarUrlBuilder {
     return this;
   }
 
+  /**
+   * Sets the time range to be used in the URL.
+   * @param {Date} fromTime the start time
+   * @param {Date} toTime the end time
+   * @returns {CarUrlBuilder} the builder instance
+   */
   withBetweenTimes(fromTime, toTime) {
     if (fromTime && toTime) {
       const fromTimeString = this._getDateString(fromTime);
@@ -72,6 +109,11 @@ export class CarUrlBuilder {
     return this;
   }
 
+  /**
+   * Sets the start time to be used in the URL.
+   * @param {Date} fromTime the start time
+   * @returns {CarUrlBuilder} the builder instance
+   */
   withFromTime(fromTime) {
     if (fromTime) {
       const fromTimeString = this._getDateString(fromTime);
@@ -81,6 +123,11 @@ export class CarUrlBuilder {
     return this;
   }
 
+  /**
+   * Sets the minimum price to be used in the URL.
+   * @param {number} price the minimum price
+   * @returns {CarUrlBuilder} the builder instance
+   */
   withFromPrice(price) {
     if (price) {
       this._addQuestionMarkIfNeeded();
@@ -89,6 +136,11 @@ export class CarUrlBuilder {
     return this;
   }
 
+  /**
+   * Sets the maximum price to be used in the URL.
+   * @param {number} price the maximum price
+   * @returns {CarUrlBuilder} the builder instance
+   */
   withToPrice(price) {
     if (price) {
       this._addQuestionMarkIfNeeded();
