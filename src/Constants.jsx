5a117d8903cd0381056255a8a03955028bb620e7
Kristian Garder <kristian@.ntnu.no>
2025-05-15 11:28:38 +0200

    Temporary experimental fix for apiurl

--- src/Constants.jsx
+++ src/Constants.jsx
@@ -6,4 +6,5 @@ const dev = {
   API_URL: "http://localhost:8080",
 }
 
-export default process.env.NODE_ENV === "production" ? prod : dev;
+export default prod;
+//export default process.env.NODE_ENV === "production" ? prod : dev;
