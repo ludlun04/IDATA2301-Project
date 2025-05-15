d456fd57f6fdb442f95725db14e92d7003c8e49f
Ludvik <ludvik.lund.hole@gmail.com>
2025-05-15 14:31:17 +0200

    car card now requests smaller resolution image

--- src/components/CarCard/CarCard.jsx
+++ src/components/CarCard/CarCard.jsx
@@ -27,7 +27,7 @@ export default function CarCard(props) {
   useEffect(() => {
     async function fetchCarImage() {
       try {
-        const imageData = await ImageAPI.getImageData(car.getId(), "jpg", 1600);
+        const imageData = await ImageAPI.getImageData(car.getId(), "jpg", 800);
 
         if (imageData && imageData.data) {
           const imageUrl = `data:image/jpeg;base64,${imageData.data}`;
